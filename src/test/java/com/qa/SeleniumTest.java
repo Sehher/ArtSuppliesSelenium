22d6f1b83e463a06fb282305bd71f932e7881d42
Sehher <sehher.tariq@academytrainee.com>
2019-09-26 14:37:15 +0100

    create test done

--- src/test/java/com/qa/SeleniumTest.java
+++ src/test/java/com/qa/SeleniumTest.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 
 public class SeleniumTest {
@@ -32,7 +31,7 @@ public class SeleniumTest {
     @Test
     public void deleteDisplayTest() throws InterruptedException {
         driver.get("http://34.89.69.35/index.html");
-        Thread.sleep(1000); //This keeps the window open for 2 seconds before closing it
+        Thread.sleep(1000);
         WebElement deleteButton = driver.findElementByXPath("//*[@id=\"cardList\"]/div[1]/div[2]/button[1]");
         Thread.sleep(1000);
         assertTrue(deleteButton.isDisplayed());
@@ -41,13 +40,65 @@ public class SeleniumTest {
     @Test
     public void deleteTest() throws InterruptedException {
         driver.get("http://34.89.69.35/index.html");
-        Thread.sleep(1000); //This keeps the window open for 2 seconds before closing it
-        WebElement deleteButton = driver.findElementByXPath("//*[@id=\"cardList\"]/div[1]/div[2]/button[1]");
+        Thread.sleep(1000);
+        WebElement addButton = driver.findElementByXPath("/html/body/center/button");
+        Thread.sleep(1000);
+        addButton.click();
+        Thread.sleep(1000);
+        WebElement productName = driver.findElementByXPath("//*[@id=\"InputName\"]");
+        assertTrue(productName.isDisplayed());
+        productName.sendKeys("Oil Paint");
+        Thread.sleep(1000);
+        WebElement categoryName = driver.findElementByXPath("//*[@id=\"InputCategory\"]");
+        categoryName.sendKeys("Paint");
+        Thread.sleep(1000);
+        WebElement description = driver.findElementByXPath("//*[@id=\"InputDescription\"]");
+        description.sendKeys("Slow drying Paint");
+        Thread.sleep(1000);
+        WebElement url = driver.findElementByXPath("//*[@id=\"InputImageLink\"]");
+        url.sendKeys("https://imgs.michaels.com/MAM/assets/1/726D45CA1C364650A39CD1B336F03305/img/85DCA835551945A5B976482AF84ED9DB/10452898_2_u.jpg?fit=inside|1024:1024");
+        Thread.sleep(1000);
+        WebElement submit = driver.findElementByXPath("/html/body/center/div[2]/div/div/div[2]/form/button");
+        submit.click();
+        Thread.sleep(1000);
+        WebElement close = driver.findElementByXPath("//*[@id=\"myModal\"]/div/div/div[1]/button");
+        close.click();
+        Thread.sleep(1000);
+        WebElement deleteButton = driver.findElementByXPath("//*[@id=\"cardList\"]/div/div[2]/button[1]");
         Thread.sleep(1000);
         deleteButton.click();
         Thread.sleep(1000);
+
+        assertFalse(deleteButton.ifPresent());
     }
 
+    @Test
+    public void createTest() throws InterruptedException {
+        driver.get("http://34.89.69.35/index.html");
+        Thread.sleep(1000);
+        WebElement addButton = driver.findElementByXPath("/html/body/center/button");
+        Thread.sleep(1000);
+        addButton.click();
+        Thread.sleep(1000);
+        WebElement productName = driver.findElementByXPath("//*[@id=\"InputName\"]");
+        assertTrue(productName.isDisplayed());
+        productName.sendKeys("Oil Paint");
+        Thread.sleep(1000);
+        WebElement categoryName = driver.findElementByXPath("//*[@id=\"InputCategory\"]");
+        categoryName.sendKeys("Paint");
+        Thread.sleep(1000);
+        WebElement description = driver.findElementByXPath("//*[@id=\"InputDescription\"]");
+        description.sendKeys("Slow drying Paint");
+        Thread.sleep(1000);
+        WebElement url = driver.findElementByXPath("//*[@id=\"InputImageLink\"]");
+        url.sendKeys("https://imgs.michaels.com/MAM/assets/1/726D45CA1C364650A39CD1B336F03305/img/85DCA835551945A5B976482AF84ED9DB/10452898_2_u.jpg?fit=inside|1024:1024");
+        Thread.sleep(1000);
+        WebElement submit = driver.findElementByXPath("/html/body/center/div[2]/div/div/div[2]/form/button");
+        submit.click();
+        Thread.sleep(1000);
+        WebElement header = driver.findElementByXPath("//*[@id=\"cardList\"]/div/div[1]/div[2]/h4");
+        assertEquals("Oil Paint",header.getText());
+    }
 
 
 
